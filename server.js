ac578d3df9c6aaa8a375737de2340a52d9c392dc
Jacob Humphreys <jacob_humphreys@icloud.com>
2025-05-16 23:20:07 -0600

    update

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongodb = require('./data/database');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("./data/database");
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use('/', require('./routes/index'));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/", require("./routes/index"));
 
 mongodb.initDatabase((err) => {
   if (err) {
     console.log(err);
   } else {
-    console.log('Connected to database');
-    app.listen(port, () => {console.log(`Server is running on port ${port}`);});
+    console.log("Connected to database");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   }
 });
-
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require('swagger-autogen')();
+const doc = {
+  info: {
+    title: 'User API',
+    description: 'API for managing users',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/index.js'];
+
+//this will generate the swagger_output.json file
+swaggerAutogen(outputFile, endpointsFiles, doc);
